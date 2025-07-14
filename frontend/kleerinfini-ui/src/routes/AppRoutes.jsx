9857d3d337ebd2ce5aa5280948be673a0f2f4ad4
Amina Otmane Telba <aminaotmanetelba@gmail.com>
2025-07-14 22:00:18 +0100

    Update AppRoutes.jsx

--- frontend/kleerinfini-ui/src/routes/AppRoutes.jsx
+++ frontend/kleerinfini-ui/src/routes/AppRoutes.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProducerProfile from '../pages/ProducerProfile';
 import ProducerRegister from '../pages/ProducerRegister';
+import Homepage from '../pages/Homepage';
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Homepage />} />
         <Route path="/producer/:id" element={<ProducerProfile />} />
         <Route path="/producer-inscription" element={<ProducerRegister />} />
       </Routes>
